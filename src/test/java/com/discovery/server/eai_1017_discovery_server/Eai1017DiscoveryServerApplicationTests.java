afed4164c0e07f738b335dbabbefa4b4f059f3af
Mahesh-74 <mahesh74.java@gmail.com>
2024-05-26 22:45:35 +0530

    FM

---
+++ src/test/java/com/discovery/server/eai_1017_discovery_server/Eai1017DiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.discovery.server.eai_1017_discovery_server;
+/*
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Eai1017DiscoveryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}*/
